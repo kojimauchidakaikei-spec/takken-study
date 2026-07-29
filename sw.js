25cbf3cf39d49df0144dce09a0b2f636b5e95c9a
kojimauchidakaikei-spec <kojima.uchidakaikei@gmail.com>
2026-07-29 12:15:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,36 @@
-const CACHE='takken-study-v010';
+const CACHE='takken-study-v020';
 const ASSETS=['./','./index.html','./manifest.json'];
 
-self.addEventListener('install',e=>
+self.addEventListener('install',e=>{
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(c=>c.addAll(ASSETS))
-  )
-);
+  );
+});
 
-self.addEventListener('activate',e=>
+self.addEventListener('activate',e=>{
   e.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(
-        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then(keys=>
+        Promise.all(
+          keys
+            .filter(k=>k!==CACHE)
+            .map(k=>caches.delete(k))
+        )
       )
-    )
-  )
-);
+    ])
+  );
+});
 
-self.addEventListener('fetch',e=>
+self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  )
-);
+    fetch(e.request)
+      .then(r=>{
+        const c=r.clone();
+        caches.open(CACHE).then(x=>x.put(e.request,c));
+        return r;
+      })
+      .catch(()=>caches.match(e.request))
+  );
+});
